02ae9d364e01753b72f3c795efde99c9a123479f
Navin Agarwal <naagar@microsoft.com>
2020-08-19 16:50:44 -0700

    Preserve the document id of each demo component's factory in a particular session.

--- src/fluidRendering/fluidLoader.tsx
+++ src/fluidRendering/fluidLoader.tsx
@@ -5,8 +5,12 @@
 import React, { useEffect, useRef } from 'react';
 import { v4 as uuid } from "uuid";
 import { IFluidCodeDetails } from "@fluidframework/container-definitions";
+import { Container } from '@fluidframework/container-loader';
 import { getLoader, getFluidObject, RouteOptions } from './loader';
 import { renderFluidDataObjects } from './renderer';
+
+// A cache of the document id per demo component's factory.
+const documentIdCache = new Map<any, string>();
 
 export const FluidLoader = (props: React.PropsWithChildren<any>) => { 
     const fluidNodeRef = useRef<HTMLDivElement>(null);
@@ -15,7 +19,6 @@ export const FluidLoader = (props: React.PropsWithChildren<any>) => {
         async function render() {
             // Add loader creation code here
             const fluidExport = props.factory;
-            const documentId = uuid();
             const codeDetails: IFluidCodeDetails = {
                 package: "storybook-pkg",
                 config: {},
@@ -23,22 +26,41 @@ export const FluidLoader = (props: React.PropsWithChildren<any>) => {
             // Empty string means get the default DataObject from container
             // You could also grab a specific component from the container using "nameOfDataObject"
             // For example in DiceRoller you could use "@fluid-example/dice-roller"
-            const dataObjectName = ""; 
+            const dataObjectName = "";
             let mode: RouteOptions = { mode: "local" };
 
-            // Create a Loader and a Container for the first client.
+            // Check if we already have created a document for this Fluid factory in this session. If so, load the
+            // existing document instead of creating a new one.
+            let createDocument: boolean = false;
+            let documentId = documentIdCache.get(props.factory);
+            if (documentId === undefined) {
+                // A document does not exist for this session. Create a documentId to be used to create a new document
+                // and set the 'createDocument' flag to true.
+                documentId = uuid();
+                documentIdCache.set(props.factory, documentId);
+                createDocument = true;
+            }
+
+            let container1: Container;
+            // Create a Loader for the first client.
             const { loader: loader1, urlResolver: urlResolver1 } = await getLoader(fluidExport, documentId, mode);
-            const container1 = await loader1.createDetachedContainer(codeDetails);
-            const attachUrl1 = await urlResolver1.createRequestForCreateNew(documentId);
-            await container1.attach(attachUrl1);
+            // If the 'createDocument' flag is set, create a new container. Otherwise, load an existing one with the 'documentId'.
+            if (createDocument) {
+                container1 = await loader1.createDetachedContainer(codeDetails);
+                const documentUrl1 = await urlResolver1.createRequestForCreateNew(documentId);
+                await container1.attach(documentUrl1);
+            } else {
+                const documentLoadUrl = `${window.location.origin}/${documentId}`;
+                container1 = await loader1.resolve({ url: documentLoadUrl });
+            }
 
             // Get the Fluid DataObject from the first Container.
             const fluidDataObject1 = await getFluidObject(container1, dataObjectName);
 
-            // Create a second Loader and get the Container with 'documentId'.
+            // Create a second Loader and load the Container with 'documentId'.
             const { loader: loader2, urlResolver: urlResolver2 } = await getLoader(fluidExport, documentId, mode);
-            const attachUrl2 = await urlResolver2.getAbsoluteUrl(container1.resolvedUrl, "");
-            const container2 = await loader2.resolve({ url: attachUrl2 });
+            const documentLoadUrl2 = await urlResolver2.getAbsoluteUrl(container1.resolvedUrl, "");
+            const container2 = await loader2.resolve({ url: documentLoadUrl2 });
 
             // Get the Fluid DataObject from the second Container.
             const fluidDataObject2 = await getFluidObject(container2, dataObjectName);

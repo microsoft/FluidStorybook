2d97c18c88e89de4631bba16002bd579c020c3da
Dan Wahlin <dwahlin@xmlforasp.net>
2020-09-02 09:48:41 -0700

    Update Storybook build script for Hugo template and cache-scripts feature (#40)
    * Fix source code path typo
    
    * Add Meta import, fix brainstorm css issue, shrink gif
    
    * Remove unncessary dependencies, minor version updates
    
    * Update build script to handle Hugo template & JS cache script
    
    * Remove unneeded comment
    
    Co-authored-by: Dan Wahlin <danwahlin@iMac-Office.local>

--- hugo-build-script.js
+++ hugo-build-script.js
@@ -1,43 +1,109 @@
 const fs = require('fs');
 const inputPath = __dirname + '/storybook-static/index.html';
+const iFramePath = __dirname + '/storybook-static/iframe.html';
 const outputPath = __dirname + '/storybook-static/playground.html';
+const cacheScriptsPath = __dirname + '/storybook-static/cache-scripts.js';
 
-fs.readFile(inputPath, (err, inputContent) => {
+fs.readFile(inputPath, (err, indexContent) => {
     if (err) {
       throw err; 
     }
-    if (inputContent) {
-        extractAndGenerateContent(inputContent);
+
+    // Read in index.html content
+    if (indexContent) {
+        // Read in iframe to grab appropriate scripts to cache
+        fs.readFile(iFramePath, (err, iFrameContent) => {
+            if (err) {
+              throw err; 
+            }
+            extractAndGenerateContent(indexContent, iFrameContent);
+        });
     }
     else {
         console.log('Unable to find index.html file');
     }
 });
 
-function extractAndGenerateContent(inputContent) {
-    let stylesheetLinks = '';
-    let body = '';
-    const content = inputContent.toString().match(/<head>(?<head>.*?)<\/head>\s*<body>(?<body>.*?)<\/body>/s);
-    const stylesheetLinksContent = content.groups.head.match(/<link\s+rel="stylesheet".*?(>|\/>|<\/link>)/gs);
+function extractAndGenerateContent(indexContent, iFrameContent) {
+    let stylesheetLinksContent;
+    let scriptMatches;
+    let iFrameScriptMatches;
+    const content = indexContent.toString().match(/<head>(?<head>.*?)<\/head>\s*<body>(?<body>.*?)<\/body>/s);
+    if (content && content.groups) {
+        stylesheetLinksContent = content.groups.head.match(/<link\s+rel="stylesheet".*?(>|\/>|<\/link>)/gs);
+        scriptMatches = Array.from(content.groups.body.matchAll(/<script\s+src="(?<src>.*?)"><\/script>/g), 
+                                        match => match.groups.src);
+        iFrameScriptMatches = Array.from(iFrameContent.toString().matchAll(/<script\s+src="(?<src>.*?)"><\/script>/g), 
+                                        match => match.groups.src);
+    }
+    else {
+        const errorMsg = 'Error finding content.groups used for body or head';
+        console.log(errorMsg);
+        throw new Error(errorMsg);
+    }
 
+    const { stylesheetLinks, body } = cleanupContent(stylesheetLinksContent, content);
+
+    // Create playground.html
+    createPlaygroundHtml(stylesheetLinks, body);
+
+    // Create cache-script.js
+    createCacheScriptsJS(scriptMatches, iFrameScriptMatches);
+}
+
+function createCacheScriptsJS(scriptMatches, iFrameScriptMatches) {
+    let scripts = [];
+    getScripts(scriptMatches, scripts);
+    getScripts(iFrameScriptMatches, scripts);
+    const cacheScript = createJSCacheScript(scripts);
+    fs.writeFile(cacheScriptsPath, cacheScript, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log(`CREATED: ${cacheScriptsPath}`);
+    });
+}
+
+function getScripts(scriptMatches, scripts) {
+    if (scriptMatches && scriptMatches.length) {
+        for (const script of scriptMatches) {
+            // We're only interested in scripts that start with vendors~main. 
+            // Others are pretty small and can be loaded on-demand.
+            if (script.startsWith('vendors~main')) {
+                scripts.push(script);
+            }
+        }
+    }
+}
+
+function createPlaygroundHtml(stylesheetLinks, body) {
+    const outputContent = createHugoTemplate(stylesheetLinks, body);
+    fs.writeFile(outputPath, outputContent, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log(`CREATED: ${outputPath}`);
+    });
+}
+
+function cleanupContent(stylesheetLinksContent, content) {
+    let stylesheetLinks;
+    let body;
     if (stylesheetLinksContent && stylesheetLinksContent.length && content.groups) {
         stylesheetLinks = stylesheetLinksContent.join('\n');
         // Remove any unnecessary white space (not required - but easier to read output)
         body = content.groups.body.replace(/\n|\s{2,}/gs, '');
+
+        // Replace root path with /playground so that https://site.com/playground and https://site.com/playground/ both work
+        stylesheetLinks = stylesheetLinks.replace(/href="/g, 'href="/playground/');
+        body = body.replace(/src="(\.\/)?/g, 'src="/playground/');
     }
     else {
         const errorMsg = 'Error finding stylesheets or body content!';
         console.log(errorMsg);
         throw new Error(errorMsg);
     }
-
-    const outputContent = createHugoTemplate(stylesheetLinks, body);
-    fs.writeFile(outputPath, outputContent, (err) => {
-        if (err) {
-            throw err;
-        }
-        console.log(`UPDATED: ${outputPath}`);
-    });
+    return { stylesheetLinks, body };
 }
 
 function createHugoTemplate(stylesheetLinks, bodyContent) {
@@ -56,4 +122,19 @@ ${bodyContent}
 {{- end }}
  
 {{ end }}`;
+}
+
+function createJSCacheScript(hashedBundles) {
+    return `// Cache scripts for performance
+if (location.href.indexOf('/playground') === -1) {
+    var scripts = [ 'sb_dll/storybook_ui_dll.js', 'sb_dll/storybook_docs_dll.js', '${hashedBundles.join('\',\'')}' ];
+    for (var i=0;i<scripts.length;i++) {
+        var name = scripts[i];
+        var script = document.createElement('script');
+        script.id = name;
+        script.async = true;
+        script.src = '/playground/' + name;
+        document.body.appendChild(script);
+    }
+}`;
 }

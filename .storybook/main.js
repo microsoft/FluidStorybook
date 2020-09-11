668218a39a4a7cfb904b41a1b9be110fdbdb1c12
Dan Wahlin <danwahlin@iMac-Office.local>
2020-09-11 13:08:23 -0700

    Add src view build into webpack for local builds

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,8 @@
  * Licensed under the MIT License.
  */
 
+const HookShellScriptPlugin = require('hook-shell-script-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.*'],
   addons: [
@@ -15,6 +17,13 @@ module.exports = {
     baseConfig.mode = options.configType.toLowerCase();
     baseConfig.optimization.minimize = isProd;
     baseConfig.devtool = isProd ? undefined : "inline-source-map";
+
+    // Add hook to run src viewer builder (creates rawSources.json)
+    // Used for local builds to keep files in-sync
+    baseConfig.plugins.push(new HookShellScriptPlugin({
+      afterEmit: ['node src-viewer-build.js']
+    }));
+    
     return baseConfig;
   }
 };
